807ae8dabf2b612101eccbc39c1243df9012ebf0
Dangloctai <tailoc2072004@gmail.com>
2025-02-07 00:05:35 +0700

    push lan 3

--- B/B.cpp
+++ B/B.cpp
@@ -28,23 +28,48 @@ typedef vector<int> vi;
 struct sinhVien{
     string ten, ngay_sinh, que_quan;
     double gpa;
-    void output(){
-        cout << ten << ' ' << ngay_sinh << ' ' << que_quan << ' ' << gpa << endl;;
+    sinhVien(){
+        ten = "nguyen van A";
+        ngay_sinh = "nn/xx/yyyy";
+        que_quan = "abcd";
+        gpa = 3.2;
+    }
+    sinhVien(string name, string ngay, string que, double diem){
+        ten = name;
+        ngay_sinh = ngay;
+        que_quan = que;
+        gpa = diem;
+    }
+};
+
+struct SoPhuc{
+    int thuc, ao;
+    SoPhuc operator + (SoPhuc other){
+        SoPhuc res;
+        res.thuc = thuc + other.thuc;
+        res.ao = ao + other.ao;
+        return res;
     }
 };
 
 //==============================Call================================
 void out(sinhVien a);
 void in(sinhVien &b);
+long long ucln(long long a, long long b);
+void xuat(SoPhuc a);
+SoPhuc sum_sophuc(SoPhuc a, SoPhuc b);
 
 //==============================Code================================
 int main(){
     fast();
-    sinhVien x;
-    sinhVien y = {"dang loc tai", "20/07/2004", "quang nam" , 3.2};
-    in(x);
-    x.output();
-    out(y);
+    // sinhVien x = {"dang loc tai", "20/07/2004", "quang nam", 3.2};
+    // sinhVien y;
+    // out(x);
+    // in(y);
+    // out(y);
+    SoPhuc a = {1, 2}, b = {8, 9};
+    SoPhuc c = a + b;
+    xuat(c);
     return 0;
 }
 
@@ -59,5 +84,25 @@ void in(sinhVien &b){
     cin.ignore();
     getline(cin, b.que_quan);
     cin >> b.gpa;
+    cin.ignore();
 }
 
+void xuat(SoPhuc a){
+    cout << a.thuc << "+" << a.ao << "i";
+}
+
+SoPhuc sum_sophuc(SoPhuc a, SoPhuc b){
+    SoPhuc res;
+    res.thuc = a.thuc + b.thuc;
+    res.ao = a.ao + b.ao;
+    return res;
+}
+
+long long ucln(long long a, long long b){
+    while(b != 0){
+        long long du = a % b;
+        a = b;
+        b = du;
+    }
+    return a;
+}
