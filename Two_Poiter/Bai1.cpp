c7afc34316882128b9b60da40dc664d1b38fe296
Tailoc078 <dangloctai00@gmail.com>
2025-02-09 18:16:06 +0700

    Two_Pointer

---
+++ Two_Poiter/Bai1.cpp
@@ -0,0 +1,60 @@
+//==============================Lib=================================
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <sstream>
+
+
+using namespace std;
+
+//==============================Define==============================
+#define fast() std::ios::sync_with_stdio(false)
+#define ifor(i, a, b) for(int i = a; i <= b; i++)
+#define dfor(i, a, b) for(int i = a; i >= b; i--)
+#define INF 1e9 + 2804
+#define all(a) a.begin(), a.end()
+#define sz(a) int((a).size())
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+//==============================Data_Type===========================
+
+
+//==============================Call================================
+
+
+//==============================Code================================
+int main(){
+    fast();
+    int n, s;
+    cin >> n >> s;
+    int a[n];
+    for(int &x : a){
+    	cin >> x;
+	}
+	int res = 0, sum = 0;
+	int l = 0, r = 0;
+	while(r < n){
+		sum += a[r];
+		while(sum > s){
+			sum -= a[l];
+			l++;
+		}
+		res = max(res, r - l  + 1);
+		r += 1;
+	}
+	cout << res;
+    return 0;
+}
+
+//==============================Function============================
+
